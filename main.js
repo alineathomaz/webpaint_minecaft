e2877330dbadac9963c0c1735096660a9e2f9c8f
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-11-12 22:39:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,184 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+playerX = 10; //coordenada x inicial para o personagem como 10 e a armazena na variável (a imagem inicia à esquerda do canvas)
+playerY = 10; //coordenada y inicial para o personagem como 10 e a armazena na variável (a imagem inicia no topo do cavas)
+
+blockImageWidth = 30; //define a width inicial da imagem dos blocos como 30 e a armazena na variável
+blockImageHeight = 30; //define a height inicial da imagem dos blocos como 30 e a armazena na variável
+
+var playerObject= ""; //variável para armazenar o objeto da imagem do personagem
+var blockImageObject= "";
+
+function playerUpdate() //função para adicionar a imagem do personagem, enviando a imagem do jogador no canvas
+//playerUpdate é o nome da função)
+{
+	fabric.Image.fromURL("player.png", function(Img) { //fabric é o nome da biblioteca que usamos, image diz que enviamos uma imagem
+		//fromURL contém a URL da imagem e a função para enviar imagens, player.png é a imagem e function(Img) é a função que enviará
+		//a imagem ao canvas
+	playerObject = Img; //variável para armazenar o objeto padrão da imagem
+
+	playerObject.scaleToWidth(150);
+	playerObject.scaleToHeight(140);
+	playerObject.set({
+	top:playerY,
+	left:playerX
+	});
+	canvas.add(playerObject);
+
+	});
+}
+
+function newImage(getImage)
+{
+	fabric.Image.fromURL(getImage, function(Img) {
+	blockImageObject = Img;
+
+	blockImageObject.scaleToWidth(blockImageWidth);
+	blockImageObject.scaleToHeight(blockImageHeight);
+	blockImageObject.set({
+	top:playerY,
+	left:playerX
+	});
+	canvas.add(blockImageObject);
+
+	});
+
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+if(e.shiftKey == true && keyPressed == '80')
+{
+	console.log("p e shift pressionadas juntas");
+	blockImageWidth = blockImageWidth + 10;
+	blockImageHeight = blockImageHeight + 10;
+	document.getElementById("currentWidth").innerHTML = blockImageWidth;
+	document.getElementById("currentHeight").innerHTML = blockImageHeight;	
+}
+if(e.shiftKey && keyPressed == '77')
+{
+	console.log("m e shift pressionadas juntas");
+	blockImageWidth = blockImageWidth - 10;
+	blockImageHeight = blockImageHeight - 10;
+	document.getElementById("currentWidth").innerHTML = blockImageWidth;
+	document.getElementById("currentHeight").innerHTML = blockImageHeight;
+}
+
+	if(keyPressed == '38')
+	{
+		up();
+		console.log("cima");
+	}
+	if(keyPressed == '40')
+	{
+		down();
+		console.log("baixo");
+	}
+	if(keyPressed == '37')
+	{
+		left();
+		console.log("esquerda");
+	}
+	if(keyPressed == '39')
+	{
+		right();
+		console.log("direita");
+	}
+	if(keyPressed == '87')
+	{
+		newImage('wall.jpg'); 
+		console.log("w");
+	}
+	if(keyPressed == '71')
+	{
+		newImage('ground.png'); 
+		console.log("g");
+	}
+	if(keyPressed == '76')
+	{
+		newImage('light_green.png'); 
+		console.log("l");
+	}
+	if(keyPressed == '84')
+	{
+		newImage('trunk.jpg'); 
+		console.log("t");
+	}
+	if(keyPressed == '82')
+	{
+		newImage('roof.jpg'); 
+		console.log("r");
+	}
+	if(keyPressed == '89')
+	{
+		newImage('yellow_wall.png'); 
+		console.log("y");
+	}
+	if(keyPressed == '68')
+	{
+		newImage('dark_green.png'); 
+		console.log("d");
+	}
+	if(keyPressed == '85')
+	{
+		newImage('unique.png'); 
+		console.log("u");
+	}
+	if(keyPressed == '67')
+	{
+		newImage('cloud.jpg'); 
+		console.log("c");
+	}
+	
+}
+function up()
+{
+	if(playerY >=0)
+	{
+		playerY = playerY - blockImageHeight;
+		console.log("altura da imagem do bloco = " + blockImageHeight);
+		console.log("Quando a tecla direcional Cima for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
+}
+
+function down()
+{
+	if(playerY <=500)
+	{
+		playerY = playerY + blockImageHeight;
+		console.log("altura da imagem do bloco = " + blockImageHeight);
+		console.log("Quando a tecla direcional Baixo for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
+}
+
+function left()
+{
+	if(playerX >0)
+	{
+		playerX = playerX - blockImageWidth;
+		console.log("largura da imagem do bloco = " + blockImageWidth);
+		console.log("Quando a tecla direcional Esquerda for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
+}
+
+function right()
+{
+	if(playerX <=850)
+	{
+		playerX = playerX + blockImageWidth;
+		console.log("largura da imagem do bloco = " + blockImageWidth);
+		console.log("Quando a tecla direcional Direita for pressionada, X =  " + playerX + " , Y = "+playerY);
+		canvas.remove(playerObject);
+		playerUpdate();
+	}
+}
